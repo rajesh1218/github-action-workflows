6e5292a165e9a3b12b04ed3d9137fa1870415b08
rajesh1218 <117444063+rajesh1218@users.noreply.github.com>
2026-01-24 19:44:05 +0530

    Add Terraform variables for Azure configuration

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "resource_group_name" {
+  default = "rg-github-actions-demo"
+}
+variable "location" {
+  default = "East US"
+}
