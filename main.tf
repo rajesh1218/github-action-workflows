aaa482c7f4f2d7ff8da1d5a0f6b63b0cc5211ccb
rajesh1218 <117444063+rajesh1218@users.noreply.github.com>
2026-01-28 17:52:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,26 @@
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-devops-demo"
+  location = "East US"
 }
 
 module "acr" {
-  source = "./modules/acr"
-  acr_name            = var.acr_name
-  resource_group_name = azurerm_resource_group.example.name
-  location            = var.location
+  source              = "./modules/acr"
+  acr_name            = "rajeshacr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 }
 
 module "app_service" {
-  source = "./modules/app_service"
-  app_name            = var.app_name
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  acr_login_server    = module.acr.login_server
-  acr_username        = module.acr.acr_admin_username
-  acr_password        = module.acr.acr_admin_password
-  docker_image_name   = var.docker_image_name
+  source              = "./modules/app_service"
+  app_name            = "rajeshapp"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  acr_login_server    = module.acr.acr_login_server
+  acr_username        = "rajeshacr" # or use azurerm_container_registry.acr.admin_username
+  acr_password        = "supersecret" # or use azurerm_container_registry.acr.admin_password
+  image_name          = "tic-tac-toe"
 }
