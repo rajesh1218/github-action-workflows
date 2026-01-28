b9b459e48a5eb00a0c279c5d6cdccb2289d04540
rajesh1218 <117444063+rajesh1218@users.noreply.github.com>
2026-01-28 15:05:05 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,19 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "4.58.0"
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-state"
+    storage_account_name = "tfstateaccount7899"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
+
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
