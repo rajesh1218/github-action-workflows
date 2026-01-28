1fba447d1a4cdcaa5f640a7d40dfb25011ccb8bc
rajesh1218 <117444063+rajesh1218@users.noreply.github.com>
2026-01-28 17:31:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ module "app_service" {
   source = "./modules/app_service"
   app_name            = var.app_name
   resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example
+  location            = azurerm_resource_group.example.location
   acr_login_server    = module.acr.login_server
   acr_username        = module.acr.acr_admin_username
   acr_password        = module.acr.acr_admin_password
