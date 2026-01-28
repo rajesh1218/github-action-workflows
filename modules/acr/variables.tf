5f58ef8e30895756291cd52bea73e04024ce184b
rajesh1218 <117444063+rajesh1218@users.noreply.github.com>
2026-01-28 17:51:20 +0530

    Update variables.tf

--- modules/acr/variables.tf
+++ modules/acr/variables.tf
@@ -1,14 +1,3 @@
-variable "acr_name" {
-  description = "Name of the Azure Container Registry"
-  type        = string
-}
-
-variable "location" {
-  description = "Location of the Azure Container Registry"
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "Name of the Resource Group"
-  type        = string
-}
+variable "acr_name" {}
+variable "resource_group_name" {}
+variable "location" {}
