d98cbe5ded34ddf3eefc9ac8d0ac28dc82f5c90c
rajesh1218 <117444063+rajesh1218@users.noreply.github.com>
2026-01-28 17:27:56 +0530

    Update main.tf

--- modules/acr/main.tf
+++ modules/acr/main.tf
@@ -8,3 +8,10 @@ resource "azurerm_container_registry" "acr" {
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
+output "acr_admin_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_admin_password" {
+  value = azurerm_container_registry.acr.admin_password
+}
