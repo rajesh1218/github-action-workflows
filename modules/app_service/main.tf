2b66835dd441f962dd887d1996703e3ac5c14805
rajesh1218 <117444063+rajesh1218@users.noreply.github.com>
2026-01-28 17:51:44 +0530

    Update main.tf

--- modules/app_service/main.tf
+++ modules/app_service/main.tf
@@ -1,26 +1,29 @@
 resource "azurerm_app_service_plan" "asp" {
-  name = "${var.app_name}-plan"
+  name                = "${var.app_name}-plan"
+  location            = var.location
   resource_group_name = var.resource_group_name
-  location = var.location
-  kind = "Linux"
-  reserved = true
+  kind                = "Linux"
+  reserved            = true
+
   sku {
-    tier = "Free"
-    size = "F1"
-  } 
+    tier = "Basic"
+    size = "B1"
+  }
 }
 
 resource "azurerm_app_service" "app" {
-    name = var.app_name
-    location = var.location
-    resource_group_name = var.resource_group_name
-    app_service_plan_id = azurerm_app_service_plan.asp.id
-    site_config {
-      linux_fx_version = "DOCKER|${var.acr_login_server}/${var.docker_image_name}:latest"
-      }
-    app_settings = {
-      "DOCKER_REGISTRY_SERVER_URL"      = "https://${var.acr_login_server}"
-      "DOCKER_REGISTRY_SERVER_USERNAME" = var.acr_username
-      "DOCKER_REGISTRY_SERVER_PASSWORD" = var.acr_password
-    }
+  name                = var.app_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+
+  site_config {
+    linux_fx_version = "DOCKER|${var.acr_login_server}/${var.image_name}:latest"
+  }
+
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL"      = "https://${var.acr_login_server}"
+    "DOCKER_REGISTRY_SERVER_USERNAME" = var.acr_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD" = var.acr_password
+  }
 }
